aa2b0a9087df9838b20e506e7e727a9f41b9aff3
Luis <luispaulo.ni@gmail.com>
2024-07-03 16:44:06 -0300

    added route to the iluminadossemsegredos content

---
+++ src/app/iluminadossemsegredos/page.tsx
@@ -0,0 +1,45 @@
+import { Biography } from '@/components/Biography'
+import { Certified } from '@/components/Certified'
+import { Header } from '@/components/Header'
+import { ModuleCollection } from '@/components/ModuleCollection'
+import { ResultCollection } from '@/components/ResultCollection'
+import { PaymentPlan } from '@/components/PaymentPlan'
+import { Footer } from '@/components/Footer'
+
+import { modules } from '@/data/modulesData'
+import { paymentData } from '@/data/paymentData'
+import { resultCollectionData } from '@/data/resultCollectionData'
+import { footerData } from '@/data/footerData'
+import { certifiedData } from '@/data/certifiedData'
+import { headerData } from '@/data/headerData'
+import { biographyData } from '@/data/biographyData'
+import { FeedbackCollection } from '@/components/FeedbackCollection'
+import { feedbackData } from '@/data/feedbackData'
+
+export default function IluminadosSemSegredos() {
+    return (
+        <>
+            <Header
+                course={headerData.course}
+                description={headerData.description}
+            />
+            <ResultCollection
+                results={resultCollectionData.images}
+                title={resultCollectionData.title}
+            />
+            <ModuleCollection modules={modules} />
+            <Certified
+                heading={certifiedData.heading}
+                img={certifiedData.img}
+                description={certifiedData.description}
+            />
+            <Biography biography={biographyData} />
+            <PaymentPlan
+                value={paymentData.value}
+                benefits={paymentData.benefits}
+            />
+            <FeedbackCollection feedbacks={feedbackData} />
+            <Footer name={footerData.name} medias={footerData.medias} />
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,4 @@
-import { Biography } from '@/components/Biography'
-import { Certified } from '@/components/Certified'
-import { Header } from '@/components/Header'
-import { ModuleCollection } from '@/components/ModuleCollection'
-import { ResultCollection } from '@/components/ResultCollection'
-import { PaymentPlan } from '@/components/PaymentPlan'
-import { Footer } from '@/components/Footer'
-
-import { modules } from '@/data/modulesData'
-import { paymentData } from '@/data/paymentData'
-import { resultCollectionData } from '@/data/resultCollectionData'
-import { footerData } from '@/data/footerData'
-import { certifiedData } from '@/data/certifiedData'
-import { headerData } from '@/data/headerData'
-import { biographyData } from '@/data/biographyData'
-import { FeedbackCollection } from '@/components/FeedbackCollection'
-import { feedbackData } from '@/data/feedbackData'
-
+import { redirect } from 'next/navigation'
 export default function Home() {
-    return (
-        <>
-            <Header
-                course={headerData.course}
-                description={headerData.description}
-            />
-            <ResultCollection
-                results={resultCollectionData.images}
-                title={resultCollectionData.title}
-            />
-            <ModuleCollection modules={modules} />
-            <Certified
-                heading={certifiedData.heading}
-                img={certifiedData.img}
-                description={certifiedData.description}
-            />
-            <Biography biography={biographyData} />
-            <PaymentPlan
-                value={paymentData.value}
-                benefits={paymentData.benefits}
-            />
-            <FeedbackCollection feedbacks={feedbackData} />
-            <Footer name={footerData.name} medias={footerData.medias} />
-        </>
-    )
+    redirect('/iluminadossemsegredos')
 }
